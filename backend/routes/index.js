3ec75b54c3fc7fe23903b976199d6e35bba94b23
carlk <carloskachuk@gmail.com>
2022-09-19 16:37:45 -0300

    routes created

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var categoryController = require('../controllers/categoryController');
+var operationController = require('../controllers/operationController');
+var authController = require('../controllers/authController');
+var userController = require('../controllers/userController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//operation routes
+
+router.get('/api/operations', operationController.getBalance);
+
+router.get('/api/operation/:idoperation', operationController.getOperation);
+
+router.post('/api/operation', operationController.createOperation);
+
+router.delete('/api/operation/:idoperation', operationController.deleteOperation);
+
+router.post('/api/operation/:idoperation', operationController.updateOperation);
+
+
+//category routes
+
+router.get('/api/categories', categoryController.getCategories);
+
+router.post('/api/category', categoryController.createCategory);
+
+router.delete('/api/category/:idcategory', categoryController.deleteCategory);
+
+router.post('/api/category/:idcategory', categoryController.updateCategory);
+
+//auth routes
+
+router.post('/api/login', authController.postLogin);
+
+router.post('/api/signup', authController.postSignup);
 
 module.exports = router;

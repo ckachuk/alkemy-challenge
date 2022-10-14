0a568a563d28037009bf96806ce230cc77b67540
carlk <carloskachuk@gmail.com>
2022-10-13 21:20:46 -0300

    component to display the table with the operation data was implemented

--- frontend/src/components/utils/OperationTable.tsx
+++ frontend/src/components/utils/OperationTable.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import Box from '@mui/material/Box';
+import OperationActions from '../operations/OperationActions';
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 100 },
@@ -8,9 +9,23 @@ const columns: GridColDef[] = [
     { field: 'amount', headerName: 'Amount', width: 170 },
     { field: 'type', headerName: 'Type (income or expenses)', width: 200 },
     { field: 'date', headerName: 'Date', width: 170 },
-    { field: 'category', headerName: 'Category', width: 170 },    
+    { field: 'category', headerName: 'Category', width: 170 },  
+    { 
+      field: 'action', 
+      headerName: 'Acciones', 
+      width:200, 
+      disableColumnMenu: true,
+      sortable: false,
+      renderCell: ({row})=>{
+        return (
+          <>
+            <OperationActions operationInfo={row}/>
+          </>
+        )
+      }
+    }  
 ];
-  
+
 
 interface OperationTableProps{
   operations: {
@@ -19,19 +34,21 @@ interface OperationTableProps{
     amount: number,
     type: string,
     date: Date,
-    category: string
-  }[]
+    category: string,
+    operationId: string
+  }[] | undefined | null
 }
 
 function OperationTable({operations}: OperationTableProps) {
   const rows = operations ? operations: [] 
   return (
-    <Box sx={{ height: 400, width: '100%' }}>
+    <Box sx={{ minHeight: 500, minWidth: 1350 }}>
     <DataGrid
+      sx={{backgroundColor: 'white'}}
       rows={rows}
       columns={columns}
-      pageSize={5}
-      rowsPerPageOptions={[5]}
+      pageSize={10}
+      rowsPerPageOptions={[10]}
     />
   </Box>
   )

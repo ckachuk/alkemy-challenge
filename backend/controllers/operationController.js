74c28bf25aaac6430f76a396a99afe36937bd529
carlk <carloskachuk@gmail.com>
2022-10-17 09:06:22 -0300

    the endpoint of the balance has been modified by adding the async parallel and the balance between incomes and expenses

--- backend/controllers/operationController.js
+++ backend/controllers/operationController.js
@@ -3,28 +3,65 @@ var {body, validationResult} = require("express-validator");
 const jwt_decode = require('jwt-decode');
 const Operation = db.operations;
 const Category = db.categories;
+const  async = require('async')
+const { Sequelize } = require('sequelize');
 
 exports.getBalance = async(req, res, next)=>{
     const token = req.headers.authorization;
     const TokenArray = token.split(" ");
     const tokenDecoded = jwt_decode(TokenArray[1])
-    try{
-        const balance = await Operation.findAll({
-            where:{
-                userId: tokenDecoded.id
-            },
-            include: [{
-                model: Category,
-                as: 'category'
-            }],
-            limit: 10,
-        })
-
-        res.json({status:"OK", message: "Get the last 10 operations", operations: balance})
-    }catch(err){
-        console.log(err);
-        res.status(400).json({status:"FAILED", message:"Something bad happened"})
-    }
+    async.parallel({
+        operations: (callback)=>{
+            Operation.findAll({
+                    where:{
+                        userId: tokenDecoded.id
+                    },
+                    include: [{
+                        model: Category,
+                        as: 'category'
+                    }],
+                    limit: 10,
+            }).then((operations)=>{
+                callback(null, operations)
+            })
+        },
+        totalIncome: (callback)=>{
+             Operation.findAll({
+                    where:{
+                        userId: tokenDecoded.id,
+                        type: 'Income'
+                    },
+                    attributes: [
+                      [Sequelize.fn('sum', Sequelize.col('amount')), 'total_amount'],
+                    ],
+                    group: ['userId'],
+                    raw: true
+            }).then((totalIncome)=>{
+                callback(null, totalIncome)
+            })
+        },
+        totalExpense: (callback)=>{
+            Operation.findAll({
+                where:{
+                    userId: tokenDecoded.id,
+                    type: 'Expense'
+                },
+                attributes: [
+                  [Sequelize.fn('sum', Sequelize.col('amount')), 'total_amount'],
+                ],
+                group: ['userId'],
+                raw: true
+            }).then((totalExpense)=>{
+                callback(null, totalExpense)
+            })
+        }
+    },
+    function(err, results){
+        if(err){res.status(400).json({status:"FAILED", message:"Something bad happened"})}
+        const balance =  results.totalIncome[0].total_amount - results.totalExpense[0].total_amount;
+        res.json({status:"OK", message: "Get the last 10 operations", operations: results.operations, balance: balance})
+    })
+    
     
 }
 

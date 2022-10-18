bac0e1e5d21fa1f092695e88b98014024f7bf45f
carlk <carloskachuk@gmail.com>
2022-10-18 07:17:27 -0300

    adding interfaces to the operations and navbar folders

--- frontend/src/components/interfaces/appInterfaces.ts
+++ frontend/src/components/interfaces/appInterfaces.ts
@@ -61,6 +61,7 @@ export interface Operation{
       id: string,
       name: string
     }
+    operationId?: string
 }
   
 export interface OperationsTable{
@@ -81,3 +82,81 @@ export interface ServerResponseCreateCategory{
 export interface Category{
     name: string
 }
+
+export interface NavBarProps{
+    currentUser:{
+        token: string | null,
+        user: {
+            username: string
+        } | null
+    } | null,
+    setCurrentUserToNull: () => void
+}
+
+
+export interface OperationActionsProps{
+  operationInfo:{
+    concept: string,
+    amount: number,
+    type: string,
+    date: Date,
+    category: string,
+    operationId: string
+  }
+}
+
+export interface ServerResponseDeleteOperation{
+  status?: string,
+  message?: string,
+}
+
+export interface ServerResponseOperation{
+    status?: string,
+    message?: string,
+    operation?: {
+        concept: string,
+        amount: number,
+        type: string,
+        date: Date,
+        category: {
+            id: string,
+            name: string,
+        },
+        categoryId: string
+    }
+}
+
+export interface CategoriesSelect {
+    value: string,
+    label: string
+}
+
+export interface ObjectCategory{
+    id: string, 
+    name: string
+}
+
+export interface IOperationForm{
+    concept: string,
+    amount: number,
+    type: string,
+    date: Date,
+    categoryId: string
+}
+
+
+export interface OperationInfoModalProps{
+    operationInfo:{
+        concept: string,
+        amount: number,
+        type: string,
+        date: Date,
+        category: string,
+    }
+  }
+
+export interface ServerResponseGetOperations{
+    status? : string,
+    message? : string,
+    operations? : Operation[]
+} 
